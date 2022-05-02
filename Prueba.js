81ec58baa124605e773a4025b431e1ca1320b1ed
Lucas Gyori <legyori@gmail.com>
2022-05-02 17:46:03 +0200

    Intento de hacer cuenta atras

--- Prueba.js
+++ Prueba.js
@@ -2,3 +2,23 @@ document.getElementById("empezar").addEventListener("click", agregarUno);
 function agregarUno() {
     this.innerHTML ++;
 }
+let btnEmpezar = document.getElementById("btnEmpezar");
+btnEmpezar.onclick = updateClock;//prompt("Cuánto tiempo tendrás?");
+function inicializarContador () {
+    updateClock;
+    btnEmpezar.style.setProperty("disabled");
+}
+
+function updateClock() {
+    let totalTime = 10;
+    for (let i = 0; i < totalTime; i--) {
+        document.getElementById('contador').innerHTML = i;
+        if(totalTime==0){
+            alert("Fin");
+            //const cuantosClicks = document.getElementById("empezar");
+        }else{
+            setTimeout("updateClock()",1000);
+        }
+        
+    }
+}
