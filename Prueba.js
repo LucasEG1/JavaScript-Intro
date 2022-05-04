57ec36c9f28ab8659f7eec7001594859dcd97e74
Lucas Gyori <legyori@gmail.com>
2022-05-04 21:46:02 +0200

    TODO validar input tiempo y mostrar mensaje

--- Prueba.js
+++ Prueba.js
@@ -1,24 +1,38 @@
-document.getElementById("empezar").addEventListener("click", agregarUno);
+document.getElementById("cajaClicks").addEventListener("click", agregarUno);
+
 function agregarUno() {
     this.innerHTML ++;
 }
-let btnEmpezar = document.getElementById("btnEmpezar");
-btnEmpezar.onclick = updateClock;//prompt("Cuánto tiempo tendrás?");
-function inicializarContador () {
-    updateClock;
-    btnEmpezar.style.setProperty("disabled");
+let inTiempo = document.querySelector("#tiempo");
+inTiempo.addEventListener("input", validarInput);
+
+let btnEmpezar = document.querySelector("#btnEmpezar");
+btnEmpezar.addEventListener("click", mostrarContenido);
+
+function mostrarContenido() {
+    let okInput;
+    let okTiempo;
+
+    okInput = validarInput;
+    if (okInput) {
+        okTiempo = confirm("El tiempo introducido es: " + inTiempo.value + ", correcto?");
+    } else {
+        okTiempo = false;
+        alert("Debes introducir un tiempo para empezar.");
+    }
+
+    if (okTiempo == true){
+        btnEmpezar.disable = true;
+    }
+
 }
 
-function updateClock() {
-    let totalTime = 10;
-    for (let i = 0; i < totalTime; i--) {
-        document.getElementById('contador').innerHTML = i;
-        if(totalTime==0){
-            alert("Fin");
-            //const cuantosClicks = document.getElementById("empezar");
-        }else{
-            setTimeout("updateClock()",1000);
-        }
-        
+function validarInput(event){
+    let tiempo = parseInt(inTiempo.value);
+    console.log(tiempo);
+    if (event.target != "" || event.target > 0) {
+        return true;
+    } else {
+        return false;
     }
 }
