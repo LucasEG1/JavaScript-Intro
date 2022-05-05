47504614bb8e109f06ac372f8362aafabb864b99
Lucas Gyori <legyori@gmail.com>
2022-05-05 20:05:21 +0200

    mensahe

--- Prueba.js
+++ Prueba.js
@@ -1,38 +1,42 @@
-document.getElementById("cajaClicks").addEventListener("click", agregarUno);
+let cajaClicks = document.querySelector("#cajaClicks");
+cajaClicks.addEventListener("click", agregarUno);
 
 function agregarUno() {
     this.innerHTML ++;
 }
+
 let inTiempo = document.querySelector("#tiempo");
 inTiempo.addEventListener("input", validarInput);
+
+function validarInput(e){
+    
+    let tiempo = parseInt(inTiempo.value);
+    console.log(tiempo);
+    /*if (inTiempo.value != NaN || inTiempo.value > 0) {
+        return true;
+    } else {
+        return false;
+    }*/
+
+}
 
 let btnEmpezar = document.querySelector("#btnEmpezar");
 btnEmpezar.addEventListener("click", mostrarContenido);
 
 function mostrarContenido() {
-    let okInput;
     let okTiempo;
+    let okInput = validarInput("click");
+    let edad;
 
-    okInput = validarInput;
-    if (okInput) {
+    if (okInput == true) {
         okTiempo = confirm("El tiempo introducido es: " + inTiempo.value + ", correcto?");
     } else {
-        okTiempo = false;
+        okTiempo == false;
         alert("Debes introducir un tiempo para empezar.");
     }
 
-    if (okTiempo == true){
+    /*if (okTiempo == true){
         btnEmpezar.disable = true;
-    }
+    }*/
 
-}
-
-function validarInput(event){
-    let tiempo = parseInt(inTiempo.value);
-    console.log(tiempo);
-    if (event.target != "" || event.target > 0) {
-        return true;
-    } else {
-        return false;
-    }
 }
