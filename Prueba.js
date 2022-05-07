6c7ae66516cde219aa70661fbaf922aaf81448af
Lucas Gyori <legyori@gmail.com>
2022-05-07 16:20:03 +0200

    arreglado boton de confirmar

--- Prueba.js
+++ Prueba.js
@@ -8,16 +8,10 @@ function agregarUno() {
 let inTiempo = document.querySelector("#tiempo");
 inTiempo.addEventListener("input", validarInput);
 
-function validarInput(e){
+function validarInput(event){
+    //VALIDAR INPUT DEBE MODIFICAR SI ES CORRECTO O NO CON CSS (como el ultimo form)
+   
     
-    let tiempo = parseInt(inTiempo.value);
-    console.log(tiempo);
-    /*if (inTiempo.value != NaN || inTiempo.value > 0) {
-        return true;
-    } else {
-        return false;
-    }*/
-
 }
 
 let btnEmpezar = document.querySelector("#btnEmpezar");
@@ -25,18 +19,23 @@ btnEmpezar.addEventListener("click", mostrarContenido);
 
 function mostrarContenido() {
     let okTiempo;
-    let okInput = validarInput("click");
-    let edad;
+    let okInput;
+
+    let tiempo = parseInt(inTiempo.value);
+    if (isNaN(tiempo) || tiempo < 1) {
+        okInput = false;
+    } else {
+        okInput = true;
+    }
 
     if (okInput == true) {
         okTiempo = confirm("El tiempo introducido es: " + inTiempo.value + ", correcto?");
     } else {
-        okTiempo == false;
-        alert("Debes introducir un tiempo para empezar.");
+        alert("Debes introducir un tiempo válido para empezar.");
     }
 
-    /*if (okTiempo == true){
+    if (okTiempo == true){
         btnEmpezar.disable = true;
-    }*/
+    }
 
 }
